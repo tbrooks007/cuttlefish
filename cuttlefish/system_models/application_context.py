5773890bfe01adc51ba9813d174edca471968115
Teresa Brooks <teresa.brooks@gmail.com>
2016-12-20 14:48:22 -0500

    added apsparser to parse cmd line arguments and added loading yaml config from cmd and using it

--- cuttlefish/system_models/application_context.py
+++ cuttlefish/system_models/application_context.py
@@ -2,5 +2,8 @@
 
 class ApplicationContext(object):
 
-    def __init__(self, nn_config, tf_config):
-        pass
+    def __init__(self, nn_config, training_config, aws_config):
+
+        self.nn_config = nn_config
+        self.training_config = training_config
+        self.aws_config = aws_config
